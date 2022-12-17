92e159ca8ead5955e77d176db384398327cf4a80
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-18 01:35:50 +0300

    add Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :users, only: %i[new create]
+  resource :sessions, only: %i[new create show destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

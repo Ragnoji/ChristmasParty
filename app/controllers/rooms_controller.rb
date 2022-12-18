5f485ed7a6eb9d36cd029db3c1ed260a46ac8b5d
w1nway <lauren_gg@mail.ru>
2022-12-18 12:58:38 +0300

    views for rooms P.2

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -14,6 +14,7 @@ class RoomsController < ApplicationController
   end
 
   def edit
+    @cities = City.all
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_17_210134) do
+ActiveRecord::Schema.define(version: 2022_12_17_210202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2022_12_17_210134) do
     t.string "last_name"
     t.citext "email"
     t.string "password_digest"
-    t.string "role", default: "member", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true

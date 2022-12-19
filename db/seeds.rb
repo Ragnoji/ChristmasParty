967697990e1082536dbca8b5d6f45be19f943880
w1nway <lauren_gg@mail.ru>
2022-12-19 14:24:17 +0300

    Members -> rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@cities = City.create([{ name: 'Kazan' }, { name: 'Moscow' }]) 
+cities = City.create([{ name: 'Kazan' }, { name: 'Moscow' }]) 
